9d9e0ae6d5c7dc3e06ab2e67a27d2ce97d0f0bab
lenayoo <lenayoo17@gmail.com>
2024-02-05 23:11:45 +0900

    today's learning

---
+++ src/pages/github.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const github = () => {
+  return (
+    <div>
+      github
+      <div>
+        <h1>How to create new repository and project</h1>
+      </div>
+    </div>
+  );
+};
