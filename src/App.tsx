67cac9008aeed991bca3b2ad932286f88b8a4c7f
lenayoo <lenayoo17@gmail.com>
2024-02-13 22:40:13 +0900

    fix some error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import { GitHub } from "./pages/Github";
-import { LearnReact } from "./pages/React";
 
 function App() {
   return (
     <>
       <h1>Essential FE knowledge⛵️</h1>
-      <div>
-        <LearnReact />
-        <GitHub />
-      </div>
+      <div></div>
     </>
   );
 }
--- src/pages/React.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const LearnReact = () => {
-  return <div>React</div>;
-};
