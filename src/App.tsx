873bfb703e15efc58312aace062730b6a36febcd
lenayoo <lenayoo17@gmail.com>
2024-02-13 23:26:37 +0900

    finish outline

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,23 @@ function App() {
   return (
     <>
       <div className="header">
-        <h1>M.TECH BLOG⛵️</h1>
+        <h1>☁︎ M. BLOG ☁︎</h1>
       </div>
-      <div>
-        <div className="navbar">navbar</div>
-        <div className="content">main content</div>
+      <div className="main">
+        <div className="navbar">
+          <ul>
+            <li>React (4)</li>
+            <li>TypeScript (1)</li>
+            <li>JavaScript (1)</li>
+            <li>Node.js (0)</li>
+            <li>GitHub (3)</li>
+            <li>Library (1)</li>
+          </ul>
+        </div>
+        <div className="content">
+          <h3>Title</h3>
+          <p>Content</p>
+        </div>
       </div>
     </>
   );
