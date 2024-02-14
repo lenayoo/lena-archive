bc316908431468d47934cdc6404da58de41583d2
lenayoo <lenayoo17@gmail.com>
2024-02-14 20:38:33 +0900

    fix css again

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="header">
-        <h1>☁︎ M. BLOG ☁︎</h1>
-      </div>
-      <div className="main">
-        <div className="navbar">
-          <ul>
-            <li>React (4)</li>
-            <li>TypeScript (1)</li>
-            <li>JavaScript (1)</li>
-            <li>Node.js (0)</li>
-            <li>GitHub (3)</li>
-            <li>Library (1)</li>
-          </ul>
+      <Router>
+        <div className="header">
+          <h1>☁️ FE TECH BLOG ☁️</h1>
         </div>
-        <div className="content">
-          <h3>Title</h3>
-          <p>Content</p>
+        <div className="main">
+          <div className="navbar">
+            <ul>
+              <li>
+                <Link to="/react">React (4)</Link>
+              </li>
+              <li>
+                <Link to="/typescript">TypeScript (1)</Link>
+              </li>
+              <li>
+                <Link to="/javascript">JavaScript (1)</Link>
+              </li>
+              <li>
+                <Link to="/nodejs">Node.js (0)</Link>
+              </li>
+              <li>
+                <Link to="/github">GitHub (3)</Link>
+              </li>
+              <li>
+                <Link to="/library">Library (1)</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="content">
+            <h3>Title</h3>
+            <p>Content</p>
+          </div>
         </div>
-      </div>
+      </Router>
     </>
   );
 }
--- src/Pages/React.tsx
+++ src/Pages/React.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function React() {
   return <div>React</div>;
 }
