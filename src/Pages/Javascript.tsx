64658ff6601228926ab5dd6d1861323a0f60dcb8
lenayoo <lenayoo17@gmail.com>
2024-02-27 07:29:40 +0900

    add javascript

--- src/Pages/Javascript.tsx
+++ src/Pages/Javascript.tsx
@@ -1,5 +1,16 @@
-function Javascript() {
-  return <div>Javascript</div>;
-}
+import Form from "../Components/Form";
+import Header from "../Components/Header";
+
+const Javascript = () => {
+  return (
+    <>
+      <Header headerTitle="JS rules the world" />
+      <Form
+        containerTitle="Array.isArray"
+        containerContent="return boolean whether value is an array or not"
+      />
+    </>
+  );
+};
 
 export default Javascript;
