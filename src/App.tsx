57f85e27090362ed8d6262f3a5a84fee71394d48
lenayoo <lenayoo17@gmail.com>
2024-03-13 20:40:32 +0900

    add reactquery

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,35 @@ import "./App.css";
 import "./output.css";
 import InputContainer from "./Pages/InputContainer";
 import { Test } from "./Pages/Test";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Router>
-      <div className="main">
-        <div className="navbar">
-          <ul>
-            <li>
-              <Link to="/inputcontainer">Input</Link>
-            </li>
-            <li>
-              <Link to="/test">TEST</Link>
-            </li>
-          </ul>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="main">
+          <div className="navbar">
+            <ul>
+              <li>
+                <Link to="/inputcontainer">Input</Link>
+              </li>
+              <li>
+                <Link to="/test">TEST</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="content w-full bg-[url('/sunset.png')] bg-no-repeat bg-cover">
+            <Routes>
+              <Route path="/inputcontainer" element={<InputContainer />} />
+              <Route path="/test" element={<Test />} />
+            </Routes>
+          </div>
         </div>
-        <div className="content w-full bg-[url('/sunset.png')] bg-no-repeat bg-cover">
-          <Routes>
-            <Route path="/inputcontainer" element={<InputContainer />} />
-            <Route path="/test" element={<Test />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Router>
+    </QueryClientProvider>
   );
 }
 
